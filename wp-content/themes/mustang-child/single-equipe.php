eed46529f1df53ce328045dd9f4c72e582a1700d
cathimon <athimon.christophe@wanadoo.fr>
2016-05-14 17:11:33 +0200

    update plugin askimet + content datas

--- wp-content/themes/mustang-child/single-equipe.php
+++ wp-content/themes/mustang-child/single-equipe.php
@@ -2,18 +2,14 @@
 <?php if (have_posts()) : ?>
   <?php while (have_posts()) : the_post(); ?>
     <article class="projet">
-      
-      <h1 class="title">
-        <?php the_title(); ?>
-      </h1>
       <div class="content">
         <?php the_content(); ?>
-      
-<?php
-tablepress_print_table( array( 'id' => '2', 'use_datatables' => true, 'print_name' => true, 'cache_table_output=false' ) );
-?>
-<?php the_post_thumbnail( 'large' ); ?>
-</div>
+      	<!-- 
+		<?php
+			tablepress_print_table( array( 'id' => '2', 'use_datatables' => true, 'print_name' => true, 'cache_table_output=false' ) );
+		?>
+		 -->
+		</div>
     </article>
   <?php endwhile; ?>
 <?php endif; ?>
