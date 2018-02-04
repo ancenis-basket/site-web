281c02f96808a2140c8a45ca6d3b7ca9a3c5237e
ATHIMON-08172 <ATHIMON-08172@D107501A248607S.cnamts.local>
2018-02-04 18:38:22 +0100

    wordpress's update

--- wp-config.php
+++ wp-config.php
@@ -19,13 +19,13 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'pabwp');
+define('DB_NAME', 'pab');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'pab');
+define('DB_USER', 'root');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'pab');
+define('DB_PASSWORD', '');
 
 /** Adresse de l'hébergement MySQL. */
 define('DB_HOST', 'localhost');
